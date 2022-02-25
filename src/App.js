d4e81a5d96824e9b01f25e277c126964a7fc8461
Jordan Dillard <jordandillard2023@u.northwestern.edu>
2022-02-25 13:38:54 -0800

    Add basic article structure

--- src/App.js
+++ src/App.js
@@ -3,21 +3,63 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="Interactive">
+      <div className="Headline">
+        <p>Why do Western audiences hate non-English-speaking artists...</p>
+      </div>
+      <div className="Lede">
+        <p>Lede lede lede lede lede lede lede lede lede lede <br></br>
+        Lede lede lede lede lede lede lede lede lede lede <br></br>
+        Lede lede lede lede lede lede lede lede lede lede <br></br>
+        Lede lede lede lede lede lede lede lede lede lede <br></br>
+        Lede lede lede lede lede lede lede lede lede lede <br></br>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="Intro">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div className="K-Pop">
+
+              <div className="BTS-Title">
+                <p>BTS</p>
+              </div>
+
+              <div className="Lyric-Comparison">
+                  <div className="Analysis">
+                    Some Analysis
+                  </div>
+                  <div className="Lyrics">
+                    'Cause I, I, I'm in the stars tonight <br></br>
+                    So watch me bring the fire and set the night alight (Hey) <br></br>
+                    Shinin' through the city with a little funk and soul <br></br>
+                    So I'ma light it up like dynamite, woah-oh-oh <br></br>
+                  </div>
+                  <div className="Lyrics">
+                    불타오르네 <br></br>
+                    (Ayy, ayy-oh, ayy-oh) <br></br>
+                    싹 다 불태워라 Bow-wow-wow <br></br>
+                    (Ayy, ayy-oh, ayy-oh) <br></br>
+                    싹 다 불태워라 Bow-wow-wow <br></br>
+                  </div>
+                  <div className="Analysis">
+                    Some Analysis
+                  </div>
+
+
+              </div>
+
+
+        </div>
+        
+      <div className="Latin-Pop">
+        
+      </div>
+
     </div>
   );
 }
